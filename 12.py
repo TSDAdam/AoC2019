4a29ef70fa6edcb153073b673a425bff66df174c
Richards <Adam.Richards@falmouth.ac.uk>
2019-12-13 12:46:14 +0000

    working on part 2

--- 12.py
+++ 12.py
@@ -12,7 +12,8 @@ for line in open('12.in').readlines():
         moon[k] = int(v)
     moons.append(moon)
     velocities.append({'x': 0, 'y': 0, 'z': 0})
-
+moons2 = moons[:]
+velocities2 = velocities[:]
 for t in range(1000):
     for moon in range(len(moons)):  # loop through moons
         for other in range(len(moons)):  # loop through other moons to compare
@@ -25,7 +26,7 @@ for t in range(1000):
         for val in moons[i]:
             moons[i][val] += velocities[i][val]
 
-
+# part 1 answer
 totalenergy = 0
 for i in range(len(moons)):
     penergy = 0
@@ -35,3 +36,43 @@ for i in range(len(moons)):
         kenergy += abs(velocities[i][val])
     totalenergy += kenergy * penergy
 print(totalenergy)
+
+# part 2
+t = 0
+z = 0
+moonval = ''
+matches = {}
+matchcounter = {}
+found = False
+while True:
+    while z < 3:
+        moons = moons2[:]
+        velocities = velocities2[:]  # take fresh copies of original positions
+        while True:
+            if z not in matches:
+                matches[str(z)] = ''
+
+            for moon in range(len(moons)):  # loop through moons
+                for other in range(len(moons)):  # loop through other moons to compare
+                    for val in moons[moon]:  # for each coordinate
+                        if moons[moon][val] < moons[other][val]:
+                            velocities[moon][val] += 1
+                        if moons[moon][val] > moons[other][val]:
+                            velocities[moon][val] -= 1
+            for i in range(len(moons)):
+                for val in moons[i]:
+                    moons[i][val] += velocities[i][val]
+            moonval = ''
+
+            for l in range(len(moons)):
+                print(moons)
+                moonval += str(moons[l][z])
+            if moonval not in matches[z]:
+                matches[z].append(moonval)
+            else:
+                matchcounter[z] = t
+                z += 1
+            t += 1  #may need to move this to the top of the loop
+    break
+
+print(matchcounter)
